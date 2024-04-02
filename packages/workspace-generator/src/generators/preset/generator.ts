d788b9f18735e486b2f8b9857deed1e0e510a08b
zpydee <benji@spawntech.io>
2024-04-02 14:00:08 +0200

    chore: spawn a nx workspace with a nextjs app

--- packages/workspace-generator/src/generators/preset/generator.ts
+++ packages/workspace-generator/src/generators/preset/generator.ts
@@ -1,4 +1,5 @@
 import {
+  addDependenciesToPackageJson,
   addProjectConfiguration,
   formatFiles,
   generateFiles,
@@ -11,15 +12,60 @@ export async function presetGenerator(
   tree: Tree,
   options: PresetGeneratorSchema
 ) {
-  const projectRoot = `libs/${options.name}`;
+  const projectRoot = `.`;
   addProjectConfiguration(tree, options.name, {
     root: projectRoot,
-    projectType: 'library',
-    sourceRoot: `${projectRoot}/src`,
+    projectType: 'application',
     targets: {},
   });
   generateFiles(tree, path.join(__dirname, 'files'), projectRoot, options);
   await formatFiles(tree);
+
+  return addDependenciesToPackageJson(
+    tree,
+    {
+      next: '14.0.4',
+      react: '18.2.0',
+      'react-dom': '18.2.0',
+      tslib: '^2.3.0',
+    },
+    {
+      '@nx/eslint': '18.2.1',
+      '@nx/eslint-plugin': '18.2.1',
+      '@nx/jest': '18.2.1',
+      '@nx/js': '18.2.1',
+      '@nx/next': '18.2.1',
+      '@nx/react': '18.2.1',
+      '@nx/workspace': '18.2.1',
+      '@swc-node/register': '~1.8.0',
+      '@swc/core': '~1.3.85',
+      '@swc/helpers': '~0.5.2',
+      '@types/jest': '^29.4.0',
+      '@types/node': '18.16.9',
+      '@types/react': '18.2.33',
+      '@types/react-dom': '18.2.14',
+      '@typescript-eslint/eslint-plugin': '^7.3.0',
+      '@typescript-eslint/parser': '^7.3.0',
+      autoprefixer: '10.4.13',
+      'babel-jest': '^29.4.1',
+      eslint: '~8.57.0',
+      'eslint-config-next': '14.0.4',
+      'eslint-config-prettier': '^9.0.0',
+      'eslint-plugin-import': '2.27.5',
+      'eslint-plugin-jsx-a11y': '6.7.1',
+      'eslint-plugin-react': '7.32.2',
+      'eslint-plugin-react-hooks': '4.6.0',
+      jest: '^29.4.1',
+      'jest-environment-jsdom': '^29.4.1',
+      nx: '18.2.1',
+      postcss: '8.4.21',
+      prettier: '^2.6.2',
+      tailwindcss: '3.2.7',
+      'ts-jest': '^29.1.0',
+      'ts-node': '10.9.1',
+      typescript: '~5.4.2',
+    }
+  );
 }
 
 export default presetGenerator;
