1abfe47740dcb93fd20306eb211bb64f221cfe62
zpydee <benji@spawntech.io>
2024-04-05 00:09:44 +0200

    feat(react-components): add all ui and typography components

---
+++ packages/react-components/src/lib/components/typography/p.tsx
@@ -0,0 +1,25 @@
+import React, { PropsWithChildren } from 'react';
+import { cn } from '../../utils';
+
+export interface IPProps
+  extends React.HTMLAttributes<HTMLParagraphElement>,
+    PropsWithChildren {
+  className?: string;
+}
+
+export const P = React.forwardRef<HTMLParagraphElement, IPProps>(
+  ({ className, ...props }, ref) => {
+    return (
+      <p
+        ref={ref}
+        className={cn(
+          'leading-7 [&:not(:first-child)]:mt-6',
+          className
+        )}
+        {...props}
+      >
+        {props.children}
+      </p>
+    );
+  }
+);
