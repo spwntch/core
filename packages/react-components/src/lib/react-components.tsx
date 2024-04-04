462024a38f57ce3b6912be75480c7bf38df90064
zpydee <benji@spawntech.io>
2024-04-04 11:44:19 +0200

    feat(react-components): add boilerplate vite react lib

---
+++ packages/react-components/src/lib/react-components.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable-next-line */
+export interface ReactComponentsProps {}
+
+export function ReactComponents(props: ReactComponentsProps) {
+  return (
+    <div>
+      <h1>Welcome to ReactComponents!</h1>
+    </div>
+  );
+}
+
+export default ReactComponents;
