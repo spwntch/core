5a1508bc55e1d4600f9e037c8027ed6f35e53d29
zpydee <benji@spawntech.io>
2024-04-05 09:53:52 +0200

    feat(next-shell): replace boilerplate with providers

---
+++ packages/next-shell/src/lib/providers/nav-provider.tsx
@@ -0,0 +1,86 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
+import { PropsWithChildren, createContext, useContext } from 'react';
+/**
+ * Represents a navigation menu in the UI library.
+ * A navigation menu can contain multiple child navigation items.
+ */
+export interface INavMenu {
+  /**
+   * The label of the navigation item.
+   */
+  label: string;
+  /**
+   * The child navigation items of the navigation menu.
+   */
+  onSelect?: (event: Event) => void;
+  children?: INavItem[];
+}
+
+/**
+ * Represents a navigation link in the UI library.
+ * A navigation link is a clickable item that navigates to a specific URL.
+ */
+export interface INavLink {
+  /**
+   * The label of the navigation item.
+   */
+  label: string;
+  description?: string;
+  /**
+   * The URL of the navigation link.
+   */
+  href: string;
+
+  /**
+   * Indicates whether the navigation link is currently active.
+   */
+  active?: boolean;
+  onSelect?: (event: Event) => void;
+}
+
+export type INavItem = INavLink | INavMenu;
+
+export const DEFAULT_NAV_CONTEXT: INavContext = {
+  path: undefined,
+  navItems: [],
+};
+
+export const NavContext = createContext<INavContext>(DEFAULT_NAV_CONTEXT);
+
+export const useNav = () => useContext(NavContext);
+
+export interface INavContext {
+  path?: string;
+  navItems: INavItem[];
+}
+
+interface INavProviderProps {
+  /**
+   * Optional navigation items to be provided.
+   */
+  navItems?: INavItem[];
+}
+
+export const NavProvider = ({
+  navItems,
+  children,
+}: INavProviderProps & PropsWithChildren) => {
+  const path = usePathname();
+
+  const context: INavContext = {
+    path,
+    navItems:
+      navItems?.map((item) => ({
+        ...item,
+        active:
+          ((item as INavLink).href &&
+            path &&
+            path.startsWith((item as INavLink).href)) ||
+          false,
+      })) || [],
+  };
+
+  return <NavContext.Provider value={context}>{children}</NavContext.Provider>;
+};
--- packages/next-shell/vite.config.ts
+++ packages/next-shell/vite.config.ts
@@ -26,23 +26,22 @@ export default defineConfig({
   // Configuration for building your library.
   // See: https://vitejs.dev/guide/build.html#library-mode
   build: {
-    outDir: '../../dist/packages/next-shell',
+    outDir: './dist',
     reportCompressedSize: true,
     commonjsOptions: {
       transformMixedEsModules: true,
     },
     lib: {
       // Could also be a dictionary or array of multiple entry points.
-      entry: 'src/index.ts',
+      entry: ['src/index.ts', 'src/server.ts'],
       name: 'next-shell',
-      fileName: 'index',
       // Change this to the formats you want to support.
       // Don't forget to update your package.json as well.
       formats: ['es', 'cjs'],
     },
     rollupOptions: {
       // External packages that should not be bundled into your library.
-      external: ['react', 'react-dom', 'react/jsx-runtime'],
+      external: ['next', 'react', 'react-dom', 'react/jsx-runtime'],
     },
   },
 });
