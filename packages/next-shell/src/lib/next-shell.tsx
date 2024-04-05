fe08e5b0a52011ecb37b449529b82fb7b446fa8f
zpydee <benji@spawntech.io>
2024-04-05 09:44:52 +0200

    feat(next-shell):adds a boilerplate nextjs lb

---
+++ packages/next-shell/src/lib/next-shell.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable-next-line */
+export interface NextShellProps {}
+
+export function NextShell(props: NextShellProps) {
+  return (
+    <div>
+      <h1>Welcome to NextShell!</h1>
+    </div>
+  );
+}
+
+export default NextShell;
