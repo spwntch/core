1c482773353523492edb0fbe37621cf3bbc0a32e
zpydee <benji@spawntech.io>
2024-04-08 10:47:30 +0200

    feat(workspace-generator): add a client component to house the regular footer from @spwntch/shell

---
+++ packages/workspace-generator/src/generators/getting-started-docs/files/components/regular-footer.tsx
@@ -0,0 +1,9 @@
+'use client';
+import { RegularFooter as SpwnRegularFooter } from '@spwntch/shell';
+import React from 'react';
+
+const RegularFooter = () => {
+  return <SpwnRegularFooter />;
+};
+
+export default RegularFooter;
