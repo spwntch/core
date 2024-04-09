3941bb1e427b6bd66841b80405bec0faf62b1090
zpydee <benji@spawntech.io>
2024-04-09 11:36:25 +0200

    feat(getting-started-docs): add tailwind support

--- apps/generator-templates/getting-started-docs/app/page.tsx
+++ apps/generator-templates/getting-started-docs/app/page.tsx
@@ -1,8 +1,3 @@
 export default function Index() {
-  /*
-   * Replace the elements below with your own.
-   *
-   * Note: The corresponding styles are in the ./index.none file.
-   */
-  return <div>Tailwind works</div>;
+  return <div className="text-5xl text-primary">Tailwind works</div>;
 }
