f908e6b52e8adeec5eb65c0e30c3a9e9743cae65
zpydee <benji@spawntech.io>
2024-04-11 10:43:03 +0200

    refactor: remove tailwind config from packages and update package.jsons

--- packages/react-components/src/index.ts
+++ packages/react-components/src/index.ts
@@ -1,5 +1,2 @@
-import '@/tailwind/tailwind.css';
-
 export * from './lib/components';
 export * from './lib/utils';
-
--- packages/react-shell/src/index.ts
+++ packages/react-shell/src/index.ts
@@ -1,3 +1,2 @@
-import '@/tailwind/tailwind.css';
 export * from './lib/components';
 export * from './lib/providers';
