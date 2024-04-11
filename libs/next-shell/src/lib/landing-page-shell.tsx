2921444252fc1e7aa776e960f51019aa9af6be80
zpydee <benji@spawntech.io>
2024-04-11 10:19:57 +0200

    feat(next-shell): next-shell now exists as a lib

---
+++ libs/next-shell/src/lib/landing-page-shell.tsx
@@ -0,0 +1,11 @@
+import { PropsWithChildren } from 'react';
+import { RegularFooter } from '@/react-shell';
+
+export const LandingPageShell = ({ children }: PropsWithChildren) => {
+  return (
+    <div>
+      {children}
+      <RegularFooter />
+    </div>
+  );
+};
