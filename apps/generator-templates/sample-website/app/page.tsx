9dbd8c30e6be13842e292a9b14e65542119f96d3
zpydee <benji@spawntech.io>
2024-04-12 11:38:55 +0200

    feat: add sample-site app

---
+++ apps/generator-templates/sample-website/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Index() {
+  /*
+   * Replace the elements below with your own.
+   *
+   * Note: The corresponding styles are in the ./index.none file.
+   */
+  return <div className="h-full flex justify-center items-center text-5xl text-primary">Hello, Tailwind</div>;
+}
