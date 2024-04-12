a15202a25d4f92ddc8bb17ebfe96f4bb0e036aa9
zpydee <benji@spawntech.io>
2024-04-12 10:30:56 +0200

    docs(next-shell): add Website Shell stories

---
+++ storybook/next/src/stories/shell/Website Shell.stories.tsx
@@ -0,0 +1,17 @@
+import { WebsiteShell } from '@/next-shell';
+import { BasicDocs, withShellProvider } from '@/react-storybook';
+import type { Meta, StoryObj } from '@storybook/react';
+
+const meta: Meta<typeof WebsiteShell> = {
+  component: WebsiteShell,
+  tags: ['autodocs'],
+  parameters: { docs: { page: BasicDocs } },
+  decorators: [withShellProvider],
+  args: {
+    // children: <UnderConstruction />,
+  },
+};
+export default meta;
+type Story = StoryObj<typeof WebsiteShell>;
+
+export const Example: Story = {};
