e90d7d0cfe2fa53b32e3cdc0298279b2ea1586d5
zpydee <benji@spawntech.io>
2024-04-16 10:57:08 +0200

    refactor(react-components): apply fix to @spwntch/shell

--- packages/react-shell/src/index.ts
+++ packages/react-shell/src/index.ts
@@ -1,3 +1,5 @@
-import '@/tailwind/styles';
+// eslint-disable-next-line @nx/enforce-module-boundaries
+import '../../tailwind/src/tailwind.css';
+
 export * from './lib/components';
 export * from './lib/providers';
