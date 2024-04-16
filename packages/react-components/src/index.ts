c20f6712a52af16f4f38b75ee64eea7aa2951bbb
zpydee <benji@spawntech.io>
2024-04-16 10:49:30 +0200

    fix(react-components): import from '../../tailwind/src/tailwind.css' to get component's displaying

--- packages/react-components/src/index.ts
+++ packages/react-components/src/index.ts
@@ -1,3 +1,4 @@
-import '@/tailwind/styles';
+// eslint-disable-next-line @nx/enforce-module-boundaries
+import '../../tailwind/src/tailwind.css';
 export * from './lib/components';
 export * from './lib/utils';
