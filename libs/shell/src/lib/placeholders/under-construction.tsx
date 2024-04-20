93c7617224ae36695e89080ee4a7d267dba95ee9
zpydee <benji@spawntech.io>
2024-04-20 22:21:29 +0200

    revertr: remove @/shell lib

--- libs/shell/src/lib/placeholders/under-construction.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-import { UnderConstruction as SpwnUnderConstruction } from '@/react-shell';
-import { PropsWithChildren } from 'react';
-
-export const UnderConstruction = (props: PropsWithChildren) => {
-  return <SpwnUnderConstruction {...props} />;
-};
