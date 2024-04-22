1a0030d402cd8ca7de7d83f93429199f546963ee
zpydee <benji@spawntech.io>
2024-04-22 10:18:20 +0200

    fix: remove pathname from under-construction

--- packages/react-shell/src/lib/components/placeholders/under-construction.tsx
+++ packages/react-shell/src/lib/components/placeholders/under-construction.tsx
@@ -1,13 +1,10 @@
 'use client';
-import { H1, H2 } from '@/react-components';
-import { usePathname } from 'next/navigation';
+import { H2 } from '@/react-components';
 import { PropsWithChildren } from 'react';
 
 export const UnderConstruction = ({ children }: PropsWithChildren) => {
-  const pathname = usePathname();
   return (
     <div className="m-4 p-4 h-full flex flex-col items-center border-4 border-dashed border-muted text-muted-foreground">
-      <H1>{pathname}</H1>
       <H2>Under Construction</H2>
       <div className="w-full pt-8 px-4 ">{children}</div>
     </div>
