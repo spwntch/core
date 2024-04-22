4b5ddf1d3f457dbe364c656811d703c02a7f1c4c
zpydee <benji@spawntech.io>
2024-04-22 12:50:46 +0200

    adds asChild directive to navtoolbarmenu navigation-menu-content

--- packages/react-shell/src/lib/components/nav/nav-toolbar/nav-toolbar-menu.tsx
+++ packages/react-shell/src/lib/components/nav/nav-toolbar/nav-toolbar-menu.tsx
@@ -34,7 +34,7 @@ export const NavToolbarMenu = ({
   return (
     <NavigationMenuItem>
       <NavigationMenuTrigger>{label} </NavigationMenuTrigger>
-      <NavigationMenuContent>
+      <NavigationMenuContent asChild>
         <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px]">
           {children?.map((item) => pickMenuItem(item))}
         </ul>
