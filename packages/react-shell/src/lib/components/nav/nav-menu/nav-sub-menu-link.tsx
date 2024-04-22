67d7062c8ee7d0d127b94f8e46e44658d4e959ce
zpydee <benji@spawntech.io>
2024-04-22 12:41:03 +0200

    fix: adds aschild prop to nav-menu-sub-link Navigation MenueIte

--- packages/react-shell/src/lib/components/nav/nav-menu/nav-sub-menu-link.tsx
+++ packages/react-shell/src/lib/components/nav/nav-menu/nav-sub-menu-link.tsx
@@ -15,7 +15,7 @@ export const NavSubMenuLink = ({
 }: INavToolbarMenuLinkProps) => {
   return (
     <li className="ml-2">
-      <NavigationMenuItem>
+      <NavigationMenuItem asChild>
         <NavigationMenuLink
           className="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foregrouns"
           onSelect={onSelect}
