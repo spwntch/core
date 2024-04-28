678d475e6f89fcbef521bb7320b6de995e928537
zpydee <benji@spawntech.io>
2024-04-28 17:11:55 +0200

    feat(react-blog): adds a post-content component complete with placeholder text

--- packages/react-blog/src/lib/components/post/post.tsx
+++ packages/react-blog/src/lib/components/post/post.tsx
@@ -3,6 +3,7 @@ import { PropsWithChildren } from 'react';
 import { PostHeader } from './post-header';
 import { IMdxDoc } from '../../types';
 import { PostToc } from './post-toc';
+import { PostContent } from './post-content';
 
 interface IPostProps {
   backTo?: { label?: string; path: string };
@@ -26,14 +27,9 @@ export const Post = ({
       </div>
       <div className="flex">
         <div className="px-2 flex-1">
-          Post Content
-          {/* <post className="max-w-none py-8 relative isolate prose text-foreground prose-headings:text-foreground">
-            {content}
-          </post> */}
+          <PostContent>{post?.content}</PostContent>
         </div>
-        <div className="w-96 p-2">
-          {<PostToc toc={post?.toc || []} />}
-        </div>
+        <div className="w-96 p-2">{<PostToc toc={post?.toc || []} />}</div>
       </div>
     </div>
   );
