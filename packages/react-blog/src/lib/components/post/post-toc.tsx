adc8b99037294d3f71c01999700ebd8ec5856d68
zpydee <benji@spawntech.io>
2024-04-28 16:23:55 +0200

    feat: every post gets a TOC, even if nothing is found

--- packages/react-blog/src/lib/components/post/post-toc.tsx
+++ packages/react-blog/src/lib/components/post/post-toc.tsx
@@ -1,4 +1,4 @@
-import { H2 } from '@/react-components';
+import { H2, H3 } from '@/react-components';
 import { ITocItem } from '../../types';
 
 type Props = {
@@ -8,8 +8,12 @@ type Props = {
 export const PostToc = ({ toc }: Props) => {
   return (
     <>
-      <H2>On this page...</H2>
-      <pre>{JSON.stringify(toc, null, 2)}</pre>
+      <H3 className="mb-4">On this page...</H3>
+      {!toc.length ? (
+        <p>Hmm... Nothing found.</p>
+      ) : (
+        <pre>{JSON.stringify(toc, null, 2)}</pre>
+      )}
     </>
   );
 };
--- packages/react-blog/src/lib/components/post/post.tsx
+++ packages/react-blog/src/lib/components/post/post.tsx
@@ -1,52 +1,39 @@
-// import { parseMdxFileBuffer } from '../utils/parse-mdx-file-buffer';
-// import { IAttributableImage, IPageSectionContent } from '@/next-shared-ui';
-
 import { IAttributableImage, IPageSectionContent } from '@/react-components';
 import { PropsWithChildren } from 'react';
 import { PostHeader } from './post-header';
+import { IMdxDoc } from '../../types';
+import { PostToc } from './post-toc';
 
 interface IPostProps {
   backTo?: { label?: string; path: string };
-  //   buffer: Buffer;
   image: IAttributableImage;
   header: IPageSectionContent;
+  post: IMdxDoc | undefined;
 }
 
 export const Post = ({
   backTo,
-  // //   buffer,
   image,
   header,
+  post,
 }: IPostProps & PropsWithChildren) => {
-  //   const doc = await parseMdxFileBuffer(buffer);
-  //   if (!doc) return notFound();
-
-  //   const { toc, content, meta } = doc;
-
-  //   console.log(toc);
-
-  //   const header: IPageSectionContent = {
-  //     heading: meta.title,
-  //     subHeading: meta.subtitle,
-  //     // tags: meta.tags,
-  //   };
+  // const { toc, content, meta } = post;
 
   return (
-    <div className="flex">
-      <div className="flex flex-col mx-2 md:mx-auto max-w-6xl">
-        <div>
-          <PostHeader backTo={backTo} image={image} header={header} />
-        </div>
-        <div className="px-2">
+    <div className="flex flex-col mx-2 md:mx-auto max-w-6xl">
+      <div>
+        <PostHeader backTo={backTo} image={image} header={header} />
+      </div>
+      <div className="flex">
+        <div className="px-2 flex-1">
           Post Content
           {/* <post className="max-w-none py-8 relative isolate prose text-foreground prose-headings:text-foreground">
             {content}
           </post> */}
         </div>
-      </div>
-      <div>
-        Post TOC
-        {/* <PostToc toc={toc} /> */}
+        <div className="w-96 p-2">
+          {<PostToc toc={post?.toc || []} />}
+        </div>
       </div>
     </div>
   );
