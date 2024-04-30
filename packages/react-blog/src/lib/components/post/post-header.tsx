58582f5c299361f64e15ca3a656b5f110387bdfd
zpydee <benji@spawntech.io>
2024-04-30 11:50:31 +0200

    feat: adds a post header

--- packages/react-blog/src/lib/components/post/post-header.tsx
+++ packages/react-blog/src/lib/components/post/post-header.tsx
@@ -1,4 +1,47 @@
+import { BasicHeader } from '@/react-components';
+import { IMdxDocMeta } from '@/react-mdx';
+import {
+  BackgroundImageHeader,
+  Button,
+  IPageSectionContent
+} from '@spwntch/components';
+import { ArrowBigLeft } from 'lucide-react';
 
-export const PostHeader = () => {
-  return <div>PostHeader</div>;
+interface IPostHeaderProps extends IMdxDocMeta {
+  backTo?: { label?: string; href: string };
+  onBackTo: (href: string) => void;
+}
+
+export const PostHeader = ({
+  backTo,
+  title,
+  subtitle,
+  tags,
+  coverImage,
+  onBackTo,
+}: IPostHeaderProps) => {
+  const header: IPageSectionContent = {
+    heading: title,
+    subHeading: subtitle,
+    tags,
+  };
+  return (
+    <div className="pt-4 sm:pt-16 pb-4">
+      {backTo && (
+        <Button
+          variant="ghost"
+          className="flex gap-2 pb-4"
+          onClick={() => onBackTo(backTo.href)}
+        >
+          <ArrowBigLeft className="h-5 w-5" aria-hidden="true" />
+          <span>{backTo.label || 'Back'}</span>
+        </Button>
+      )}
+      {coverImage ? (
+        <BackgroundImageHeader header={header} image={{ src: coverImage }} />
+      ) : (
+        <BasicHeader header={header} />
+      )}
+    </div>
+  );
 };
