bc79fd24d9d4ca9998e0b1ced4994fc835acf7a9
zpydee <benji@spawntech.io>
2024-05-02 16:21:12 +0200

    fix: optimise card-header padding

--- packages/react-components/src/lib/components/page-widgets/grid-card.tsx
+++ packages/react-components/src/lib/components/page-widgets/grid-card.tsx
@@ -28,7 +28,7 @@ export const GridCard = ({ image, content, onClick }: GridCardProps) => {
           src={image.src}
         />
       </CardContent>
-      <CardHeader className='py-0 px-3'>
+      <CardHeader className='px-3 py-2'>
         {tags?.length && (
           <ul className="hidden md:flex gap-3 mb-3 ">
             {tags.map((category, index) => (
