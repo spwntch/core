3e0472501247f0cf1b3aa6d9f21cb5ba4e7e194b
zpydee <benji@spawntech.io>
2024-05-02 11:29:52 +0200

    fix: if empty array passed into logos, return null

--- packages/react-components/src/lib/components/page-widgets/logo-cloud.tsx
+++ packages/react-components/src/lib/components/page-widgets/logo-cloud.tsx
@@ -3,6 +3,7 @@ export interface LogoCloudProps {
 }
 
 export const LogoCloud = ({ logos }: LogoCloudProps) => {
+  if (!logos?.length) return null;
   return (
     <div className="mx-auto bg-primary py-12 px-8  grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-4">
       {logos.map((logo, index) => (
--- storybook/react/src/stories/components/page-widgets/Logo Cloud.stories.tsx
+++ storybook/react/src/stories/components/page-widgets/Logo Cloud.stories.tsx
@@ -19,3 +19,6 @@ export default meta;
 type Story = StoryObj<typeof LogoCloud>;
 
 export const logoCloud: Story = {};
+export const noLogosProvider: Story = {
+  args: { logos: [] },
+};
