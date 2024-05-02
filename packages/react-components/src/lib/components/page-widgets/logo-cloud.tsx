6f7613221383eda14925a89e82fc4b3a23877c6f
zpydee <benji@spawntech.io>
2024-05-02 11:37:27 +0200

    fix: adds key to logo img

--- packages/react-components/src/lib/components/page-widgets/logo-cloud.tsx
+++ packages/react-components/src/lib/components/page-widgets/logo-cloud.tsx
@@ -8,6 +8,7 @@ export const LogoCloud = ({ logos }: LogoCloudProps) => {
     <div className="mx-auto bg-primary py-12 px-8  grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-4">
       {logos.map((logo, index) => (
         <img
+          key={logo.src}
           className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
           src={logo.src}
           alt={logo.alt}
