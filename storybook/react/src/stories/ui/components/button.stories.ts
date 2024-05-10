fea0fa51c92d599c9188d10bda5880342cc5f298
zpydee <benji@spawntech.io>
2024-05-10 13:11:12 +0200

    docs: adds a story for our temp component

---
+++ storybook/react/src/stories/ui/components/button.stories.ts
@@ -0,0 +1,14 @@
+import { Button } from '@/react-ui';
+import type { Meta, StoryObj } from '@storybook/react';
+
+const meta: Meta<typeof Button> = {
+  component: Button,
+  args: {
+    children: 'Click Me',
+  },
+  // tags: ['autodocs'],
+};
+export default meta;
+type Story = StoryObj<typeof Button>;
+
+export const button: Story = {};
