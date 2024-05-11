bacac5cce098416e545bed321730e110bc9bb279
zpydee <benji@spawntech.io>
2024-05-11 12:19:25 +0200

    build: vite now splits css and injects per component

--- packages/react-ui/vite.config.ts
+++ packages/react-ui/vite.config.ts
@@ -1,10 +1,14 @@
 /// <reference types='vitest' />
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 import react from '@vitejs/plugin-react';
-import * as path from 'path';
+import { glob } from 'glob';
+import { extname, join, relative } from 'path';
+import { fileURLToPath } from 'url';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 import { libInjectCss } from 'vite-plugin-lib-inject-css';
+
+
 
 export default defineConfig({
   root: __dirname,
@@ -15,7 +19,7 @@ export default defineConfig({
     nxViteTsPaths(),
     dts({
       entryRoot: 'src',
-      tsconfigPath: path.join(__dirname, 'tsconfig.lib.json'),
+      tsconfigPath: join(__dirname, 'tsconfig.lib.json'),
     }),
     libInjectCss(),
   ],
@@ -28,10 +32,19 @@ export default defineConfig({
     lib: {
       entry: 'src/index.ts',
       name: 'react-ui',
-      formats: ['cjs'],
+      formats: ['es'],
     },
     rollupOptions: {
       external: ['react', 'react-dom', 'react/jsx-runtime'],
+      input: Object.fromEntries(
+        glob.sync('src/**/*.{ts,tsx}').map(file => [
+          relative(
+            'src',
+            file.slice(0, file.length - extname(file).length)
+          ),
+          fileURLToPath(new URL(file, import.meta.url))
+        ])
+      ),
       output:{
         assetFileNames: 'assets/[name][extname]',
         entryFileNames: '[name].js',
