09c8bab9c2be34ed2ad843d3611c23b0c2d2ced5
zpydee <benji@spawntech.io>
2024-05-17 15:08:04 +0200

    replace fake button with shadcn/ui button

--- packages/react-ui/src/lib/components/ui/button/button.tsx
+++ packages/react-ui/src/lib/components/ui/button/button.tsx
@@ -1,2 +1,51 @@
+import { Slot } from '@radix-ui/react-slot';
+import { cva, type VariantProps } from 'class-variance-authority';
+import * as React from 'react';
 import styles from './button.module.css';
-export const Button = () => <button className={styles.button}>click</button>;
+
+import { cn } from '../../../utils';
+
+const buttonVariants = cva(styles.button, {
+  variants: {
+    variant: {
+      primary: styles.primary,
+      secondary: styles.secondary,
+      destructive: styles.destructive,
+      ghost: styles.ghost,
+      outline: styles.outline,
+      link: styles.link,
+    },
+    size: {
+      sm: styles.sm,
+      md: styles.md,
+      lg: styles.lg,
+      icon: styles.icon,
+    },
+  },
+  defaultVariants: {
+    variant: 'primary',
+    size: 'md',
+  },
+});
+
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
+  asChild?: boolean;
+}
+
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, variant, size, asChild = false, ...props }, ref) => {
+    const Comp = asChild ? Slot : 'button';
+    return (
+      <Comp
+        className={cn(buttonVariants({ variant, size, className }))}
+        ref={ref}
+        {...props}
+      />
+    );
+  }
+);
+Button.displayName = 'Button';
+
+export { Button, buttonVariants };
