ed3b1cdb34df7810e2cf0517e9617f783dadb50d
zpydee <benji@spawntech.io>
2024-05-19 13:52:14 +0200

    export menubar and navigaiton menu

--- packages/react-ui/src/lib/components/ui/index.ts
+++ packages/react-ui/src/lib/components/ui/index.ts
@@ -3,3 +3,5 @@ export * from './button/button';
 export * from './badge/badge';
 export * from './card/card';
 export * from './dropdown-menu/dropdown-menu';
+export * from './menubar/menubar';
+export * from './navigation-menu/navigation-menu';
