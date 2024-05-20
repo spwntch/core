e926c09fd2e3fc714d5cc026fc15dade4119dd3d
zpydee <benji@spawntech.io>
2024-05-20 14:46:49 +0200

    card

--- packages/react-ui/src/lib/components/ui/card/card.stories.tsx
+++ packages/react-ui/src/lib/components/ui/card/card.stories.tsx
@@ -1,37 +1,52 @@
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from './card';
-import type { Meta, StoryObj } from '@storybook/react';
+import React from "react"
+import { Meta, StoryObj } from "@storybook/react"
+import { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent } from "./card"
 
 const meta: Meta<typeof Card> = {
-  title: 'UI/Components/Card',
+  title: "UI/Components/Card",
   component: Card,
-  args: {
-    children: 'Click Me',
-  },
   tags: ['autodocs'],
-};
-export default meta;
-type Story = StoryObj<typeof Card>;
+  parameters: {
+    docs: {
+      description: {
+        component: "A container for displaying content and actions on a single topic.",
+      },
+    },
+  },
+}
 
-export const basic: Story = {
-  render: () => (
-    <Card>
+export default meta
+
+export const Default: StoryObj<typeof Card> = {
+  render: (args) => (
+    <Card {...args}>
       <CardHeader>
         <CardTitle>Card Title</CardTitle>
-        <CardDescription>Card Description</CardDescription>
+        <CardDescription>This is the card description</CardDescription>
       </CardHeader>
       <CardContent>
-        <p>Card Content</p>
+        This is the main content of the card.
       </CardContent>
       <CardFooter>
-        <p>Card Footer</p>
+        Card Footer
       </CardFooter>
     </Card>
   ),
-};
+}
+
+export const WithLongContent: StoryObj<typeof Card> = {
+  render: (args) => (
+    <Card {...args}>
+      <CardHeader>
+        <CardTitle>Card Title</CardTitle>
+        <CardDescription>This is the card description</CardDescription>
+      </CardHeader>
+      <CardContent>
+        This is the main content of the card. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      </CardContent>
+      <CardFooter>
+        Card Footer
+      </CardFooter>
+    </Card>
+  ),
+}
--- packages/react-ui/src/lib/components/ui/card/card.tsx
+++ packages/react-ui/src/lib/components/ui/card/card.tsx
@@ -1,53 +1,120 @@
 import * as React from 'react';
-
 import { cn } from '../../../utils';
 import styles from './card.module.css';
 
+/**
+ * Card component - A container for displaying content and actions on a single topic.
+ * 
+ * @param {object} props - Props for Card
+ * @param {string} [props.className] - Additional className for the component
+ * @param {React.Ref} ref - Forwarded ref
+ * @returns {JSX.Element} The Card component
+ */
 const Card = React.forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement>
 >(({ className, ...props }, ref) => (
-  <div ref={ref} className={cn(styles.card, className)} {...props} />
+  <div
+    ref={ref}
+    className={cn(styles.card, className)}
+    {...props}
+  />
 ));
 Card.displayName = 'Card';
 
+/**
+ * CardHeader component - The header of the card, typically contains the title and description.
+ * 
+ * @param {object} props - Props for CardHeader
+ * @param {string} [props.className] - Additional className for the component
+ * @param {React.Ref} ref - Forwarded ref
+ * @returns {JSX.Element} The CardHeader component
+ */
 const CardHeader = React.forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement>
 >(({ className, ...props }, ref) => (
-  <div ref={ref} className={cn(styles.header, className)} {...props} />
+  <div
+    ref={ref}
+    className={cn(styles['card-header'], className)}
+    {...props}
+  />
 ));
 CardHeader.displayName = 'CardHeader';
 
+/**
+ * CardTitle component - The title of the card.
+ * 
+ * @param {object} props - Props for CardTitle
+ * @param {string} [props.className] - Additional className for the component
+ * @param {React.Ref} ref - Forwarded ref
+ * @returns {JSX.Element} The CardTitle component
+ */
 const CardTitle = React.forwardRef<
-  HTMLParagraphElement,
+  HTMLHeadingElement,
   React.HTMLAttributes<HTMLHeadingElement>
 >(({ className, ...props }, ref) => (
-  <h3 ref={ref} className={cn(styles.title, className)} {...props} />
+  <h3
+    ref={ref}
+    className={cn(styles['card-title'], className)}
+    {...props}
+  />
 ));
 CardTitle.displayName = 'CardTitle';
 
+/**
+ * CardDescription component - The description of the card.
+ * 
+ * @param {object} props - Props for CardDescription
+ * @param {string} [props.className] - Additional className for the component
+ * @param {React.Ref} ref - Forwarded ref
+ * @returns {JSX.Element} The CardDescription component
+ */
 const CardDescription = React.forwardRef<
   HTMLParagraphElement,
   React.HTMLAttributes<HTMLParagraphElement>
 >(({ className, ...props }, ref) => (
-  <p ref={ref} className={cn('', className)} {...props} />
+  <p
+    ref={ref}
+    className={cn(styles['card-description'], className)}
+    {...props}
+  />
 ));
 CardDescription.displayName = 'CardDescription';
 
+/**
+ * CardContent component - The main content area of the card.
+ * 
+ * @param {object} props - Props for CardContent
+ * @param {string} [props.className] - Additional className for the component
+ * @param {React.Ref} ref - Forwarded ref
+ * @returns {JSX.Element} The CardContent component
+ */
 const CardContent = React.forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement>
 >(({ className, ...props }, ref) => (
-  <div ref={ref} className={cn(styles.content, className)} {...props} />
+  <div ref={ref} className={cn(styles['card-content'], className)} {...props} />
 ));
 CardContent.displayName = 'CardContent';
 
+/**
+ * CardFooter component - The footer of the card, typically contains actions.
+ * 
+ * @param {object} props - Props for CardFooter
+ * @param {string} [props.className] - Additional className for the component
+ * @param {React.Ref} ref - Forwarded ref
+ * @returns {JSX.Element} The CardFooter component
+ */
 const CardFooter = React.forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement>
 >(({ className, ...props }, ref) => (
-  <div ref={ref} className={cn(styles.footer, className)} {...props} />
+  <div
+    ref={ref}
+    className={cn(styles['card-footer'], className)}
+    {...props}
+  />
 ));
 CardFooter.displayName = 'CardFooter';
 
