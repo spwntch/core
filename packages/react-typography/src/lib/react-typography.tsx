9318ce314063257a03f477aaa9e59264f9414094
zpydee <benji@spawntech.io>
2024-05-22 14:08:42 +0200

    feat: adds a boilerplate @/react-typography package

---
+++ packages/react-typography/src/lib/react-typography.tsx
@@ -0,0 +1,14 @@
+import './react-typography.css';
+
+/* eslint-disable-next-line */
+export interface ReactTypographyProps {}
+
+export function ReactTypography(props: ReactTypographyProps) {
+  return (
+    <div>
+      <h1>Welcome to ReactTypography!</h1>
+    </div>
+  );
+}
+
+export default ReactTypography;
