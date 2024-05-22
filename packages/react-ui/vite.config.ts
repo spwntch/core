2c5018640f95b6732e8c4b1adf79663a933a6a85
zpydee <benji@spawntech.io>
2024-05-22 12:25:09 +0200

    update vite inputs

--- packages/react-ui/vite.config.ts
+++ packages/react-ui/vite.config.ts
@@ -1,10 +1,12 @@
 /// <reference types='vitest' />
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 import react from '@vitejs/plugin-react';
-import { join } from 'path';
+import { extname, join, relative } from 'path';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 import { libInjectCss } from 'vite-plugin-lib-inject-css';
+import { glob } from 'glob';
+import { fileURLToPath } from 'url';
 
 export default defineConfig({
   root: __dirname,
@@ -32,14 +34,14 @@ export default defineConfig({
       formats: ['es'],
     },
     rollupOptions: {
-      // input: Object.fromEntries(
-      //   glob
-      //     .sync('src/**/*.{ts,tsx}')
-      //     .map((file) => [
-      //       relative('src', file.slice(0, file.length - extname(file).length)),
-      //       fileURLToPath(new URL(file, import.meta.url)),
-      //     ])
-      // ),
+      input: Object.fromEntries(
+        glob
+          .sync('src/**/*.{ts,tsx}')
+          .map((file) => [
+            relative('src', file.slice(0, file.length - extname(file).length)),
+            fileURLToPath(new URL(file, import.meta.url)),
+          ])
+      ),
       output: {
         assetFileNames: 'assets/[name][extname]',
         entryFileNames: '[name].js',
