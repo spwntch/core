bf448e3591f1c42ab30768a64ba631376dfd1ed9
zpydee <benji@spawntech.io>
2024-06-03 09:57:00 +0200

    try stringifying kep

--- libs/node-langchain/src/lib/openai/openai.ts
+++ libs/node-langchain/src/lib/openai/openai.ts
@@ -1,7 +1,7 @@
 import { ChatOpenAI } from '@langchain/openai';
 
 const openai = new ChatOpenAI({
-  apiKey: process.env['NEXT_PUBLIC_OPENAI_API_KEY'],
+  apiKey: `${process.env['NEXT_PUBLIC_OPENAI_API_KEY']}`,
   modelName: 'gpt-4o',
   temperature: 0.5,
   maxTokens: 1000,
