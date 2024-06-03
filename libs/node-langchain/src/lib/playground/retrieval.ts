8c80d6370efd26c9c25741783798c9c1a012154d
zpydee <benji@spawntech.io>
2024-06-03 16:52:04 +0200

    puls setup for chat into main method

--- libs/node-langchain/src/lib/playground/retrieval.ts
+++ libs/node-langchain/src/lib/playground/retrieval.ts
@@ -7,35 +7,38 @@ import { CheerioWebBaseLoader } from 'langchain/document_loaders/web/cheerio';
 import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
 import { MemoryVectorStore } from 'langchain/vectorstores/memory';
 
-const prompt = ChatPromptTemplate.fromMessages([
-  [
-    'system',
-    `
-    Return a radnomly selected joke about space-ships from your knowledge base: {context}
-    Always adda divider line (*-------LOL--------*)  below the joke, so that it is easier to read.
-    `,
-  ],
-]);
-
-const loader = new CheerioWebBaseLoader(
-  'https://icanhazdadjoke.com/search?term=space%20ship'
-);
-
-const spliter = new RecursiveCharacterTextSplitter({
-  chunkSize: 200,
-  chunkOverlap: 50,
-});
-
-const getRetriever = async (docs: any[]) => {
-  const store = await MemoryVectorStore.fromDocuments(docs, embeddings);
-  return store.asRetriever();
-};
-
-/**
- * API
- */
-
 export const retrieveDadJoke = async (): Promise<unknown> => {
+  /**
+   * SETUP
+   */
+
+  const prompt = ChatPromptTemplate.fromMessages([
+    [
+      'system',
+      `
+     Return a radnomly selected joke about space-ships from your knowledge base: {context}
+     Always adda divider line (*-------LOL--------*)  below the joke, so that it is easier to read.
+     `,
+    ],
+  ]);
+
+  const loader = new CheerioWebBaseLoader(
+    'https://icanhazdadjoke.com/search?term=space%20ship'
+  );
+
+  const spliter = new RecursiveCharacterTextSplitter({
+    chunkSize: 200,
+    chunkOverlap: 50,
+  });
+
+  const getRetriever = async (docs: any[]) => {
+    const store = await MemoryVectorStore.fromDocuments(docs, embeddings);
+    return store.asRetriever();
+  };
+
+  /**
+   * CHAIN
+   */
   const promptChain = await createStuffDocumentsChain({
     llm: openai,
     prompt,
