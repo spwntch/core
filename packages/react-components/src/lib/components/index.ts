ad046b064525d007731f39e5435672b7b05a3613
zpydee <benji@spawntech.io>
2024-06-04 14:55:05 +0200

    delete old components contents

--- packages/react-components/src/lib/components/index.ts
+++
@@ -1,8 +0,0 @@
-export * from './containers';
-export * from './media';
-export * from './page-headers';
-export * from './page-sections';
-export * from './page-widgets';
-export * from './typography';
-export * from './ui';
-
--- packages/react-components/src/lib/components/page-widgets/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './logo-cloud'
-export * from './card-grid'
-export * from './grid-card'
