c056998d06638ad4896d00dbaa4f0779a2e5948f
zpydee <benji@spawntech.io>
2024-06-04 13:41:10 +0200

    fix stories file extension

--- packages/react-ui/src/lib/components/ui/carousel/carousel.tsx
+++ packages/react-ui/src/lib/components/ui/carousel/carousel.tsx
@@ -4,7 +4,7 @@ import useEmblaCarousel, {
 } from 'embla-carousel-react';
 import { ArrowLeft, ArrowRight } from 'lucide-react';
 
-import { cn } from '@/react-ui/lib/utils';
+import { cn } from '../../../utils';
 import { Button } from '../button/button';
 
 type CarouselApi = UseEmblaCarouselType[1];
