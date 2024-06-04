746caf8602457891bf8af22cc3caf954bce04e89
zpydee <benji@spawntech.io>
2024-06-04 16:21:23 +0200

    logo-carousel working

--- packages/react-components/src/lib/components/carousels/logo-carousel/logo-carousel.tsx
+++ packages/react-components/src/lib/components/carousels/logo-carousel/logo-carousel.tsx
@@ -1,4 +1,7 @@
-import * as React from 'react';
+import { H2, H3 } from '@/react-typography';
+import { Carousel, CarouselContent, CarouselItem } from '@/react-ui';
+import Autoplay from 'embla-carousel-autoplay';
+import { useRef } from 'react';
 import styles from './logo-carousel.module.css';
 
 export interface LogoCarouselProps {
@@ -7,12 +10,19 @@ export interface LogoCarouselProps {
 }
 
 const LogoCarousel: React.FC<LogoCarouselProps> = ({ title, logos }) => {
+  if (!logos?.length) return null;
+  const carouselOptions = { loop: true, duration: 4000 };
+  const plugin = useRef(Autoplay({ delay: 0, jump: false }));
   return (
-    <div className={styles['logo-carousel']}>
-      <h2 className={styles['logo-carousel-title']}>{title}</h2>
-      <div className={styles['carousel-content']}>
+    <Carousel
+      className={styles['logo-carousel']}
+      opts={carouselOptions}
+      plugins={[plugin.current]}
+    >
+      <H2 className={styles['logo-carousel-title']}>{title}</H2>
+      <CarouselContent className={styles['carousel-content']}>
         {logos.map((logo, index) => (
-          <div key={index} className={styles['carousel-item']}>
+          <CarouselItem key={index} className={styles['carousel-item']}>
             <div className={styles['logo-item-container']}>
               <img
                 src={logo}
@@ -20,10 +30,10 @@ const LogoCarousel: React.FC<LogoCarouselProps> = ({ title, logos }) => {
                 className={styles['logo-item']}
               />
             </div>
-          </div>
+          </CarouselItem>
         ))}
-      </div>
-    </div>
+      </CarouselContent>
+    </Carousel>
   );
 };
 
