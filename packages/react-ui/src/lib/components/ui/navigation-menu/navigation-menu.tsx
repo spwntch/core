29743a9b7ac544e3617244b3ce654888cacf5877
zpydee <benji@spawntech.io>
2024-06-04 14:14:05 +0200

    navigation menu

--- packages/react-ui/src/lib/components/ui/navigation-menu/navigation-menu.tsx
+++ packages/react-ui/src/lib/components/ui/navigation-menu/navigation-menu.tsx
@@ -3,21 +3,8 @@ import * as NavigationMenuPrimitive from '@radix-ui/react-navigation-menu';
 import { cva } from 'class-variance-authority';
 import { ChevronDown } from 'lucide-react';
 
-import { cn } from '../../../utils/cn';
+import { cn } from '../../../utils';
 
-/**
- * NavigationMenu component and its subcomponents for creating a responsive navigation menu.
- *
- * The `NavigationMenu` component provides a flexible and customizable navigation menu system.
- * Use the various subcomponents to construct menus with triggers, content areas, indicators, and viewports.
- *
- * - `NavigationMenuTrigger`: The element that toggles the visibility of the menu content.
- * - `NavigationMenuContent`: The container for the menu content.
- * - `NavigationMenuItem`: A standard item within the menu.
- * - `NavigationMenuLink`: A link item within the menu.
- * - `NavigationMenuIndicator`: An indicator for the currently active menu item.
- * - `NavigationMenuViewport`: A viewport for displaying the menu content.
- */
 const NavigationMenu = React.forwardRef<
   React.ElementRef<typeof NavigationMenuPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof NavigationMenuPrimitive.Root>
@@ -36,14 +23,6 @@ const NavigationMenu = React.forwardRef<
 ));
 NavigationMenu.displayName = NavigationMenuPrimitive.Root.displayName;
 
-/**
- * NavigationMenuList component for displaying a list of navigation items.
- *
- * - Use the `NavigationMenuList` component to display a list of items within the navigation menu.
- *
- * @param {React.ComponentPropsWithoutRef<typeof NavigationMenuPrimitive.List>} props - The properties to configure the NavigationMenuList component.
- * @returns {JSX.Element} The rendered NavigationMenuList component.
- */
 const NavigationMenuList = React.forwardRef<
   React.ElementRef<typeof NavigationMenuPrimitive.List>,
   React.ComponentPropsWithoutRef<typeof NavigationMenuPrimitive.List>
@@ -65,14 +44,6 @@ const navigationMenuTriggerStyle = cva(
   'group inline-flex h-10 w-max items-center justify-center rounded-md bg-background px-4 py-2 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50'
 );
 
-/**
- * NavigationMenuTrigger component for toggling the visibility of the menu content.
- *
- * - Use the `NavigationMenuTrigger` component to display a trigger button for the navigation menu.
- *
- * @param {React.ComponentPropsWithoutRef<typeof NavigationMenuPrimitive.Trigger>} props - The properties to configure the NavigationMenuTrigger component.
- * @returns {JSX.Element} The rendered NavigationMenuTrigger component.
- */
 const NavigationMenuTrigger = React.forwardRef<
   React.ElementRef<typeof NavigationMenuPrimitive.Trigger>,
   React.ComponentPropsWithoutRef<typeof NavigationMenuPrimitive.Trigger>
@@ -91,14 +62,6 @@ const NavigationMenuTrigger = React.forwardRef<
 ));
 NavigationMenuTrigger.displayName = NavigationMenuPrimitive.Trigger.displayName;
 
-/**
- * NavigationMenuContent component for displaying menu content.
- *
- * - Use the `NavigationMenuContent` component to display the content of the navigation menu.
- *
- * @param {React.ComponentPropsWithoutRef<typeof NavigationMenuPrimitive.Content>} props - The properties to configure the NavigationMenuContent component.
- * @returns {JSX.Element} The rendered NavigationMenuContent component.
- */
 const NavigationMenuContent = React.forwardRef<
   React.ElementRef<typeof NavigationMenuPrimitive.Content>,
   React.ComponentPropsWithoutRef<typeof NavigationMenuPrimitive.Content>
@@ -116,14 +79,6 @@ NavigationMenuContent.displayName = NavigationMenuPrimitive.Content.displayName;
 
 const NavigationMenuLink = NavigationMenuPrimitive.Link;
 
-/**
- * NavigationMenuViewport component for displaying the menu content in a viewport.
- *
- * - Use the `NavigationMenuViewport` component to display the content of the navigation menu.
- *
- * @param {React.ComponentPropsWithoutRef<typeof NavigationMenuPrimitive.Viewport>} props - The properties to configure the NavigationMenuViewport component.
- * @returns {JSX.Element} The rendered NavigationMenuViewport component.
- */
 const NavigationMenuViewport = React.forwardRef<
   React.ElementRef<typeof NavigationMenuPrimitive.Viewport>,
   React.ComponentPropsWithoutRef<typeof NavigationMenuPrimitive.Viewport>
@@ -142,14 +97,6 @@ const NavigationMenuViewport = React.forwardRef<
 NavigationMenuViewport.displayName =
   NavigationMenuPrimitive.Viewport.displayName;
 
-/**
- * NavigationMenuIndicator component for displaying an indicator for the active menu item.
- *
- * - Use the `NavigationMenuIndicator` component to indicate the currently active menu item.
- *
- * @param {React.ComponentPropsWithoutRef<typeof NavigationMenuPrimitive.Indicator>} props - The properties to configure the NavigationMenuIndicator component.
- * @returns {JSX.Element} The rendered NavigationMenuIndicator component.
- */
 const NavigationMenuIndicator = React.forwardRef<
   React.ElementRef<typeof NavigationMenuPrimitive.Indicator>,
   React.ComponentPropsWithoutRef<typeof NavigationMenuPrimitive.Indicator>
