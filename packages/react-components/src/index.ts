79f41c8b0e49bcb9b9f5ec5870f3b4792e18ec1f
zpydee <benji@spawntech.io>
2024-06-04 16:47:46 +0200

    remove ttypes from @spwntch/components

--- packages/react-components/src/index.ts
+++ packages/react-components/src/index.ts
@@ -1,4 +1,3 @@
-// export * from './lib/components';
-export * from './lib/types';
+export * from './lib/components';
 export * from './lib/utils';
 
---
+++ packages/react-components/src/lib/components/index.ts
@@ -0,0 +1,6 @@
+export * from './carousels/logo-carousel/logo-carousel';
+export * from './containers/background-image-container/background-image-container';
+export * from './containers/button-group/button-group';
+export * from './grids/logo-cloud/logo-cloud';
+export * from './media/youtube-player/youtube-player';
+
