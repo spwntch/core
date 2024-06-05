560a233005c42aed39d378360f45894d9ca3b354
zpydee <benji@spawntech.io>
2024-06-05 18:06:26 +0200

    export nav types and provider

--- packages/react-shell/src/lib/components/nav/index.ts
+++ packages/react-shell/src/lib/components/nav/index.ts
@@ -2,3 +2,4 @@ export * from './desktop-top-navbar/desktop-top-navbar';
 export * from './mobile-nav-drawer/mobile-nav-drawer';
 export * from './nav-menu/nav-menu';
 export * from './nav-toolbar/nav-toolbar';
+export * from './nav-provider/nav-provider';
