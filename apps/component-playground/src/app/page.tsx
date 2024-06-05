f748396bdd0c5be99e3344ec9b8e869daef6de93
zpydee <benji@spawntech.io>
2024-06-05 11:21:04 +0200

    boilerplate component-playground app

---
+++ apps/component-playground/src/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Index() {
+  /*
+   * Replace the elements below with your own.
+   *
+   * Note: The corresponding styles are in the ./index.none file.
+   */
+  return (
+    <div className="text-red-500">hiya</div>
+  );
+}
