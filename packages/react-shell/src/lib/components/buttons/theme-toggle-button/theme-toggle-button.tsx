723aa0ae9b24050ad94224027ba93b1ac72f2e17
zpydee <benji@spawntech.io>
2024-06-05 12:16:49 +0200

    keep tailwind with dark: modifier in component

--- packages/react-shell/src/lib/components/buttons/theme-toggle-button/theme-toggle-button.tsx
+++ packages/react-shell/src/lib/components/buttons/theme-toggle-button/theme-toggle-button.tsx
@@ -7,20 +7,23 @@ import {
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
+  cn,
 } from '@/react-ui';
 import { Moon, Sun } from 'lucide-react';
 import { useTheme } from 'next-themes';
 import styles from './theme-toggle-button.module.css';
 
-export const ThemeToggleButton: React.FC<{ className?: string }> = ({ className }) => {
+export const ThemeToggleButton: React.FC<{ className?: string }> = ({
+  className,
+}) => {
   const { setTheme } = useTheme();
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="outline" size="icon" className={className}>
-          <Sun className={styles['sun-icon']} />
-          <Moon className={styles['moon-icon']} />
+          <Sun className={cn(styles['icon'], 'dark:hidden')} />
+          <Moon className={cn(styles['icon'], 'hidden dark:block')} />
           <span className={styles['screen-reader-text']}>Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
