f0939afcfa111d0af11117b6ef3e398d999c0e6b
zpydee <benji@spawntech.io>
2024-06-06 15:39:43 +0200

    seem to have quandrants working

--- packages/react-components/src/lib/components/containers/content-container/content-container.stories.tsx
+++ packages/react-components/src/lib/components/containers/content-container/content-container.stories.tsx
@@ -1,39 +1,29 @@
 import React from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
 import { ContentContainer } from './content-container';
-import {
-  withFullPage,
-  withFullWidth,
-} from '../../../storybook/storybook-decorators';
 import { Button } from '@/react-ui';
+import { withFullPage } from '../../../storybook/storybook-decorators';
 
 /**
- * The `ContentContainer` component displays various content elements such as headings, subheadings, body text, bullets, tags, and announcements.
+ * The `ContentContainer` component displays a section with a heading, sub-heading, body text, and optional bullets and tags.
  *
  * ## How to Use
  *
- * Wrap the `ContentContainer` component around any content that you want to display.
+ * Wrap the `ContentContainer` component around any content that you want to include under the section.
  *
  * ```tsx
  * import { ContentContainer } from './content-container';
  *
  * export default function App() {
- *   const content = {
+ *   const innerContent = {
  *     heading: 'Welcome to Our Site',
  *     subHeading: 'We are glad to have you here',
  *     body: ['Our site offers a variety of features to help you succeed.'],
- *     announcement: { message: 'New feature available!', cta: <Button>Learn More</Button> },
- *     bullets: [
- *       { text: 'Feature 1' },
- *       { text: 'Feature 2' },
- *       { text: 'Feature 3' },
- *     ],
- *     tags: ['Tag 1', 'Tag 2'],
  *   };
  *
  *   return (
- *     <ContentContainer innerContent={content}>
- *       <Button >Get Started</Button>
+ *     <ContentContainer innerContent={innerContent} hAlign="center" vAlign="middle">
+ *       <Button>Get Started</Button>
  *       <Button variant="secondary">Learn More</Button>
  *     </ContentContainer>
  *   );
@@ -43,30 +33,29 @@ import { Button } from '@/react-ui';
 const meta: Meta<typeof ContentContainer> = {
   title: 'components/containers/content-container',
   component: ContentContainer,
-  decorators: [withFullPage],
   tags: ['autodocs'],
+  decorators: [withFullPage],
   parameters: {
     layout: 'fullscreen',
   },
   argTypes: {
-    innerContent: {
-      description: 'The content to be displayed',
-      control: 'object',
-    },
     vAlign: {
       description: 'Vertical alignment of the content',
-      control: 'radio',
+      control: 'select',
       options: ['top', 'middle', 'bottom'],
     },
     hAlign: {
       description: 'Horizontal alignment of the content',
-      control: 'radio',
+      control: 'select',
       options: ['left', 'center', 'right'],
     },
-
     className: {
       description: 'Additional class names for the component',
       control: 'text',
+    },
+    innerContent: {
+      description: 'The content of the section',
+      control: 'object',
     },
   },
 };
@@ -86,6 +75,8 @@ export const Basic: Story = {
       subHeading: 'We are glad to have you here',
       body: ['Our site offers a variety of features to help you succeed.'],
     },
+    hAlign: 'center',
+    vAlign: 'middle',
     children: (
       <>
         <Button>Get Started</Button>
@@ -96,43 +87,177 @@ export const Basic: Story = {
 };
 
 /**
- * Left Aligned ContentContainer example.
- * Demonstrates the ContentContainer component with left-aligned text content.
+ * Top Left Aligned ContentContainer example.
+ * Demonstrates the ContentContainer component with top-left aligned text content.
  */
-// export const LeftAligned: Story = {
-//   args: {
-//     alignment: 'left',
-//     innerContent: {
-//       heading: 'Welcome to Our Site',
-//       subHeading: 'We are glad to have you here',
-//       body: ['Our site offers a variety of features to help you succeed.'],
-//     },
-//     children: (
-//       <>
-//         <Button variant="outline">Get Started</Button>
-//         <Button variant="ghost">Learn More</Button>
-//       </>
-//     ),
-//   },
-// };
+export const TopLeft: Story = {
+  args: {
+    innerContent: {
+      heading: 'Welcome to Our Site',
+      subHeading: 'We are glad to have you here',
+      body: ['Our site offers a variety of features to help you succeed.'],
+    },
+    hAlign: 'left',
+    vAlign: 'top',
+    children: (
+      <>
+        <Button>Get Started</Button>
+        <Button variant="secondary">Learn More</Button>
+      </>
+    ),
+  },
+};
 
 /**
- * Right Aligned ContentContainer example.
- * Demonstrates the ContentContainer component with right-aligned text content.
+ * Top Center Aligned ContentContainer example.
+ * Demonstrates the ContentContainer component with top-center aligned text content.
  */
-// export const RightAligned: Story = {
-//   args: {
-//     alignment: 'right',
-//     innerContent: {
-//       heading: 'Welcome to Our Site',
-//       subHeading: 'We are glad to have you here',
-//       body: ['Our site offers a variety of features to help you succeed.'],
-//     },
-//     children: (
-//       <>
-//         <Button variant="destructive">Get Started</Button>
-//         <Button variant="link">Learn More</Button>
-//       </>
-//     ),
-//   },
-// };
+export const TopCenter: Story = {
+  args: {
+    innerContent: {
+      heading: 'Welcome to Our Site',
+      subHeading: 'We are glad to have you here',
+      body: ['Our site offers a variety of features to help you succeed.'],
+    },
+    hAlign: 'center',
+    vAlign: 'top',
+    children: (
+      <>
+        <Button>Get Started</Button>
+        <Button variant="secondary">Learn More</Button>
+      </>
+    ),
+  },
+};
+
+/**
+ * Top Right Aligned ContentContainer example.
+ * Demonstrates the ContentContainer component with top-right aligned text content.
+ */
+export const TopRight: Story = {
+  args: {
+    innerContent: {
+      heading: 'Welcome to Our Site',
+      subHeading: 'We are glad to have you here',
+      body: ['Our site offers a variety of features to help you succeed.'],
+    },
+    hAlign: 'right',
+    vAlign: 'top',
+    children: (
+      <>
+        <Button>Get Started</Button>
+        <Button variant="secondary">Learn More</Button>
+      </>
+    ),
+  },
+};
+
+/**
+ * Middle Left Aligned ContentContainer example.
+ * Demonstrates the ContentContainer component with middle-left aligned text content.
+ */
+export const MiddleLeft: Story = {
+  args: {
+    innerContent: {
+      heading: 'Welcome to Our Site',
+      subHeading: 'We are glad to have you here',
+      body: ['Our site offers a variety of features to help you succeed.'],
+    },
+    hAlign: 'left',
+    vAlign: 'middle',
+    children: (
+      <>
+        <Button>Get Started</Button>
+        <Button variant="secondary">Learn More</Button>
+      </>
+    ),
+  },
+};
+
+/**
+ * Middle Right Aligned ContentContainer example.
+ * Demonstrates the ContentContainer component with middle-right aligned text content.
+ */
+export const MiddleRight: Story = {
+  args: {
+    innerContent: {
+      heading: 'Welcome to Our Site',
+      subHeading: 'We are glad to have you here',
+      body: ['Our site offers a variety of features to help you succeed.'],
+    },
+    hAlign: 'right',
+    vAlign: 'middle',
+    children: (
+      <>
+        <Button>Get Started</Button>
+        <Button variant="secondary">Learn More</Button>
+      </>
+    ),
+  },
+};
+
+/**
+ * Bottom Left Aligned ContentContainer example.
+ * Demonstrates the ContentContainer component with bottom-left aligned text content.
+ */
+export const BottomLeft: Story = {
+  args: {
+    innerContent: {
+      heading: 'Welcome to Our Site',
+      subHeading: 'We are glad to have you here',
+      body: ['Our site offers a variety of features to help you succeed.'],
+    },
+    hAlign: 'left',
+    vAlign: 'bottom',
+    children: (
+      <>
+        <Button>Get Started</Button>
+        <Button variant="secondary">Learn More</Button>
+      </>
+    ),
+  },
+};
+
+/**
+ * Bottom Center Aligned ContentContainer example.
+ * Demonstrates the ContentContainer component with bottom-center aligned text content.
+ */
+export const BottomCenter: Story = {
+  args: {
+    innerContent: {
+      heading: 'Welcome to Our Site',
+      subHeading: 'We are glad to have you here',
+      body: ['Our site offers a variety of features to help you succeed.'],
+    },
+    hAlign: 'center',
+    vAlign: 'bottom',
+    children: (
+      <>
+        <Button>Get Started</Button>
+        <Button variant="secondary">Learn More</Button>
+      </>
+    ),
+  },
+};
+
+/**
+ * Bottom Right Aligned ContentContainer example.
+ * Demonstrates the ContentContainer component with bottom-right aligned text content.
+ */
+export const BottomRight: Story = {
+  args: {
+    innerContent: {
+      heading: 'Welcome to Our Site',
+      subHeading: 'We are glad to have you here',
+      body: ['Our site offers a variety of features to help you succeed.'],
+    },
+    hAlign: 'right',
+    vAlign: 'bottom',
+    children: (
+      <>
+        <Button>Get Started</Button>
+        <Button variant="secondary">Learn More</Button>
+      </>
+    ),
+  },
+};
