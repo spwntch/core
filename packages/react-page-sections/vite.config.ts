f1f96eb9db1dd95f9a5337e8684d2d5e2ac0263e
zpydee <benji@spawntech.io>
2024-06-09 15:03:39 +0200

    moves react-content -> react-page-sections

--- packages/react-page-sections/vite.config.ts
+++ packages/react-page-sections/vite.config.ts
@@ -7,7 +7,7 @@ import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 
 export default defineConfig({
   root: __dirname,
-  cacheDir: '../../node_modules/.vite/packages/react-content',
+  cacheDir: '../../node_modules/.vite/packages/reat-page-sectiions',
 
   plugins: [
     react(),
@@ -34,7 +34,7 @@ export default defineConfig({
     lib: {
       // Could also be a dictionary or array of multiple entry points.
       entry: 'src/index.ts',
-      name: 'react-content',
+      name: 'reat-page-sectiions',
       fileName: 'index',
       // Change this to the formats you want to support.
       // Don't forget to update your package.json as well.
