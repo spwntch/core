24c2a0e51367fc9f57145a3990a4ea773cbb1692
zpydee <benji@spawntech.io>
2024-06-11 13:18:59 +0200

    article interim

--- packages/react-mdx/src/lib/components/article/article.tsx
+++ packages/react-mdx/src/lib/components/article/article.tsx
@@ -1,33 +1,27 @@
 import { IMdxDoc } from '../../types';
 import { cn } from '@/react-ui';
-import styles from './mdx-article.module.css';
+import styles from './article.module.css';
 // import { MdxArticleHeader } from '../mdx-article-header/mdx-article-header';
 // import { MdxArticleToc } from '../mdx-article-toc/mdx-article-toc';
 
-interface IPostProps extends IMdxDoc {
+interface IArticleProps extends IMdxDoc {
   backTo?: { label?: string; href: string };
   onBackTo?: (href: string) => void;
   onToc: (href: string) => void;
 }
 
-export const MdxArticle = ({
+export const Article = ({
   backTo,
   meta,
   toc,
   content,
   onBackTo,
   onToc,
-}: IPostProps) => {
+}: IArticleProps) => {
   return (
     <div className={cn(styles.container)}>
-      <div className={cn(styles.header)}>
-        HEADER HERE
-        {/* <MdxArticleHeader backTo={backTo} {...meta} onBackTo={onBackTo} /> */}
-      </div>
       <article className={cn(styles.article)}>
-        <div className={cn(styles['prose-content'])}>
-          {content}
-        </div>
+        <div className={cn(styles['prose-content'])}>{content}</div>
         {toc?.length && (
           <div className={cn(styles.toc)}>
             TOC HERE
