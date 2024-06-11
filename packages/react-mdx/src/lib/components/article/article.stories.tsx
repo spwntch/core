5a0da59348578f596b34d6670380ccfbc19c6f7a
zpydee <benji@spawntech.io>
2024-06-11 14:14:08 +0200

    article interim

--- packages/react-mdx/src/lib/components/article/article.stories.tsx
+++ packages/react-mdx/src/lib/components/article/article.stories.tsx
@@ -1,33 +1,66 @@
 import { Meta, StoryObj } from '@storybook/react';
 import { Article } from './article';
+import { articles } from '../../storybook/storybook-config';
+
+const componentDescription = `
+### Overview
+The \`Article\` component displays an article with its metadata, content, and optionally a table of contents (TOC).
+
+### Props
+- \`meta\`: The metadata of the article.
+- \`toc\`: The table of contents of the article.
+- \`content\`: The content of the article.
+- \`backTo\`: An optional object with \`label\` and \`href\` for a back button.
+- \`onBackTo\`: An optional function to handle back button clicks.
+- \`onToc\`: An optional function to handle TOC clicks.
+
+### Example
+\`\`\`
+<Article
+  meta={articles[0].meta}
+  toc={articles[0].toc}
+  content={<p>This is the content of the post.</p>}
+  onBackTo={(href) => console.log(\`Navigating to: \${href}\`)}
+  onToc={(href) => console.log(\`Navigating to: \${href}\`)}
+/>
+\`\`\`
+`;
 
 const meta: Meta<typeof Article> = {
   title: 'mdx/article',
   component: Article,
   tags: ['autodocs'],
-  argTypes: {
-    backTo: {
-      description: 'Back to navigation link',
-      control: 'object',
+  parameters: {
+    layout: 'centered',
+    docs: {
+      description: {
+        component: componentDescription,
+      },
     },
+  },
+  argTypes: {
     meta: {
-      description: 'Metadata for the post',
+      description: 'The metadata of the article',
       control: 'object',
     },
     toc: {
-      description: 'Table of contents',
-      //   control: 'array',
+      description: 'The table of contents of the article',
+      control: 'object',
     },
     content: {
-      description: 'Content of the post',
-      control: 'text',
+      description: 'The content of the article',
+      control: 'object', // Use object control for JSX content
+    },
+    backTo: {
+      description: 'An optional object with label and href for a back button',
+      control: 'object',
     },
     onBackTo: {
-      description: 'Function to handle back navigation',
+      description: 'An optional function to handle back button clicks',
       action: 'clicked',
     },
     onToc: {
-      description: 'Function to handle TOC click',
+      description: 'An optional function to handle TOC clicks',
       action: 'clicked',
     },
   },
@@ -37,23 +70,103 @@ export default meta;
 
 type Story = StoryObj<typeof Article>;
 
+/**
+ * Default Article example.
+ * Demonstrates a basic usage of the Article component with the first article.
+ */
 export const Default: Story = {
   args: {
-    backTo: { label: 'Back to Home', href: '/' },
-    // meta: {
-    //   title: 'Sample Post Title',
-    //   date: 'June 11, 2024',
-    // },
-    toc: [
-      { label: 'Introduction', href: '#introduction' },
-      { label: 'Conclusion', href: '#conclusion' },
-    ],
-    // content: '<p>This is the content of the post.</p>',
-    onBackTo: (href: string) => {
-      alert(`Back to: ${href}`);
-    },
-    onToc: (href: string) => {
-      alert(`Navigate to: ${href}`);
-    },
+    meta: articles[0].meta,
+    toc: articles[0].toc,
+    content: (
+      <>
+        <h1>The Real Reason You Need an ICP</h1>
+        <p>
+          <strong>What is an ICP?</strong>
+        </p>
+        <p>
+          An Ideal Customer Profile (ICP) is a detailed description of the
+          perfect customer for your product or service. This profile is based on
+          various factors such as demographics, behaviors, and pain points. An
+          ICP helps you focus your marketing and sales efforts on the customers
+          who are most likely to benefit from and purchase your offerings.
+        </p>
+        <p>
+          <strong>Why Do You Need an ICP?</strong>
+        </p>
+        <p>
+          The importance of having an ICP cannot be overstated. Here are some
+          key reasons why an ICP is crucial for your business:
+        </p>
+        <ul>
+          <li>
+            <strong>Targeted Marketing Efforts:</strong> An ICP allows you to
+            create highly targeted marketing campaigns. By understanding the
+            specific needs and characteristics of your ideal customers, you can
+            tailor your messages and offers to resonate with them, leading to
+            higher conversion rates.
+          </li>
+          <li>
+            <strong>Efficient Sales Processes:</strong> With an ICP, your sales
+            team can focus their efforts on the prospects who are most likely to
+            convert. This not only increases the efficiency of your sales
+            process but also improves the overall customer experience.
+          </li>
+          <li>
+            <strong>Better Product Development:</strong> Understanding your
+            ideal customer helps you build better products. You can prioritize
+            features and improvements that address the pain points and needs of
+            your target audience, leading to higher customer satisfaction and
+            loyalty.
+          </li>
+        </ul>
+        <img
+          src="/articles/cover-images/the-real-reason-you-need-an-icp.webp"
+          alt="Ideal Customer Profile"
+        />
+        <p>
+          <strong>How to Create an ICP</strong>
+        </p>
+        <p>Creating an ICP involves several steps:</p>
+        <ol>
+          <li>
+            <strong>Analyze Your Current Customers:</strong> Start by analyzing
+            your existing customers to identify common characteristics and
+            behaviors. Look for patterns in demographics, purchasing behaviors,
+            and product usage.
+          </li>
+          <li>
+            <strong>Conduct Market Research:</strong> Market research can
+            provide valuable insights into your ideal customers. Use surveys,
+            interviews, and industry reports to gather data on potential
+            customers who fit your profile.
+          </li>
+          <li>
+            <strong>Define Key Characteristics:</strong> Based on your analysis
+            and research, define the key characteristics of your ideal customer.
+            These may include demographics (age, gender, location), behaviors
+            (buying patterns, product usage), and pain points (challenges,
+            needs).
+          </li>
+          <li>
+            <strong>Create a Detailed Profile:</strong> Combine all the
+            information you've gathered to create a detailed profile of your
+            ideal customer. Include specific details such as age range, job
+            title, industry, and key challenges.
+          </li>
+        </ol>
+        <p>
+          <strong>Conclusion</strong>
+        </p>
+        <p>
+          Having an ICP is essential for any business looking to optimize its
+          marketing, sales, and product development efforts. By focusing on your
+          ideal customers, you can create more effective strategies, improve
+          customer satisfaction, and ultimately drive growth for your business.
+        </p>
+      </>
+    ),
+    onBackTo: (href) => console.log(`Navigating to: ${href}`),
+    onToc: (href) => console.log(`Navigating to: ${href}`),
   },
 };
--- packages/react-mdx/src/lib/components/article/article.tsx
+++ packages/react-mdx/src/lib/components/article/article.tsx
@@ -1,13 +1,11 @@
-import { IMdxDoc } from '../../types';
 import { cn } from '@/react-ui';
+import { IMdxDoc } from '../../types';
 import styles from './article.module.css';
-// import { MdxArticleHeader } from '../mdx-article-header/mdx-article-header';
-// import { MdxArticleToc } from '../mdx-article-toc/mdx-article-toc';
 
 interface IArticleProps extends IMdxDoc {
   backTo?: { label?: string; href: string };
   onBackTo?: (href: string) => void;
-  onToc: (href: string) => void;
+  onToc?: (href: string) => void;
 }
 
 export const Article = ({
@@ -21,12 +19,14 @@ export const Article = ({
   return (
     <div className={cn(styles.container)}>
       <article className={cn(styles.article)}>
+        {backTo && onBackTo && (
+          <button onClick={() => onBackTo(backTo.href)}>
+            {backTo.label || 'Back'}
+          </button>
+        )}
         <div className={cn(styles['prose-content'])}>{content}</div>
         {toc?.length && (
-          <div className={cn(styles.toc)}>
-            TOC HERE
-            {/* <MdxArticleToc toc={toc} onClickToc={onToc} /> */}
-          </div>
+          <div className={cn(styles.toc)}>{/* TOC content */}</div>
         )}
       </article>
     </div>
