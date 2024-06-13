1b31631dbb06d1a2d0be3acd61a2e6d638df6f02
zpydee <benji@spawntech.io>
2024-06-14 00:34:25 +0200

    trying to split up files...

--- packages/react-ui/vite.config.ts
+++ packages/react-ui/vite.config.ts
@@ -1,10 +1,12 @@
 /// <reference types='vitest' />
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 import react from '@vitejs/plugin-react-swc';
-import * as path from 'path';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 import { libInjectCss } from 'vite-plugin-lib-inject-css';
+import {glob} from 'glob';
+import { extname, join, relative } from 'path';
+import { fileURLToPath } from 'url';
 
 export default defineConfig({
   root: __dirname,
@@ -15,7 +17,7 @@ export default defineConfig({
     libInjectCss(),
     dts({
       entryRoot: 'src',
-      tsconfigPath: path.join(__dirname, 'tsconfig.lib.json'),
+      tsconfigPath: join(__dirname, 'tsconfig.lib.json'),
       exclude: ['**/*.stories.tsx', '**/*.specs.ts'],
     }),
   ],
@@ -50,6 +52,14 @@ export default defineConfig({
         'tailwind-merge',
         'tailwind-animate',
       ],
+      input: Object.fromEntries(
+        glob
+          .sync('src/**/*.{ts,tsx}')
+          .map((file) => [
+            relative('src', file.slice(0, file.length - extname(file).length)),
+            fileURLToPath(new URL(file, import.meta.url)),
+          ])
+      ),
       output: {
         assetFileNames: 'assets/[name][extname]',
         entryFileNames: '[name].mjs',
