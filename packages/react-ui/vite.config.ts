764a1dbb11ff5ff5c5cef30bae23b05c1572155e
zpydee <benji@spawntech.io>
2024-06-14 00:37:17 +0200

    revert rollup inputs & outputs

--- packages/react-ui/vite.config.ts
+++ packages/react-ui/vite.config.ts
@@ -1,12 +1,10 @@
 /// <reference types='vitest' />
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 import react from '@vitejs/plugin-react-swc';
+import { join } from 'path';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 import { libInjectCss } from 'vite-plugin-lib-inject-css';
-import {glob} from 'glob';
-import { extname, join, relative } from 'path';
-import { fileURLToPath } from 'url';
 
 export default defineConfig({
   root: __dirname,
@@ -52,18 +50,6 @@ export default defineConfig({
         'tailwind-merge',
         'tailwind-animate',
       ],
-      input: Object.fromEntries(
-        glob
-          .sync('src/**/*.{ts,tsx}')
-          .map((file) => [
-            relative('src', file.slice(0, file.length - extname(file).length)),
-            fileURLToPath(new URL(file, import.meta.url)),
-          ])
-      ),
-      output: {
-        assetFileNames: 'assets/[name][extname]',
-        entryFileNames: '[name].mjs',
-      },
     },
   },
 });
