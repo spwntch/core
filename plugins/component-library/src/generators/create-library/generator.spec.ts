0aa5139ebcdc4bdb50fc9ba02191f8a27157e7a9
zpydee <benji@spawntech.io>
2024-07-05 14:21:45 +0200

    fix tests

--- plugins/component-library/src/generators/create-library/generator.spec.ts
+++ plugins/component-library/src/generators/create-library/generator.spec.ts
@@ -1,12 +1,14 @@
+import { Tree } from '@nx/devkit';
 import { createTreeWithEmptyWorkspace } from '@nx/devkit/testing';
-import { Tree, readProjectConfiguration } from '@nx/devkit';
 
-import { createLibraryGenerator } from './generator';
 import { CreateLibraryGeneratorSchema } from './schema';
 
 describe('create-library generator', () => {
   let tree: Tree;
-  const options: CreateLibraryGeneratorSchema = { name: 'test' };
+  const options: CreateLibraryGeneratorSchema = {
+    name: 'test',
+    publishable: false,
+  };
 
   beforeEach(() => {
     tree = createTreeWithEmptyWorkspace();
