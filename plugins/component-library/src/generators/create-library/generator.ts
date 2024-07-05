4b99690126d0b41bd3dd92fc59af259c4862ba9c
zpydee <benji@spawntech.io>
2024-07-05 11:19:44 +0200

    adds file templates

--- plugins/component-library/src/generators/create-library/generator.ts
+++ plugins/component-library/src/generators/create-library/generator.ts
@@ -1,4 +1,5 @@
 import {
+  addProjectConfiguration,
   formatFiles,
   generateFiles,
   Tree
@@ -7,6 +8,7 @@ import { Linter } from '@nx/eslint';
 import { libraryGenerator } from '@nx/react';
 import { CreateLibraryGeneratorSchema } from './schema';
 import { join } from 'path';
+import { addLinter } from './lib/add-linter';
 
 export async function createLibraryGenerator(
   tree: Tree,
@@ -16,33 +18,33 @@ export async function createLibraryGenerator(
     options.directory || (options.publishable ? 'packages' : 'libs');
   const projectRoot = `${options.directory}/${options.name}`;
 
-  // addProjectConfiguration(tree, options.name, {
-  //   root: projectRoot,
-  //   projectType: 'library',
-  //   sourceRoot: `${projectRoot}/src`,
-  //   targets: {
-  //     'extract-tailwind': {
-  //       executor: '@spwntch/component-library-plugin:extract-tailwind',
-  //       options: {},
-  //     },
-  //     'add-docs': {
-  //       executor: '@spwntch/component-library-plugin:add-docs',
-  //       options: {},
-  //     },
-  //   },
-  // });
-
-  // await addLinter(tree, projectRoot, options);
-  
-  await libraryGenerator(tree, {
-    name: options.name,
-    directory: options.directory,
-    bundler: 'vite',
-    compiler: 'babel',
-    component: true,
-    linter: Linter.EsLint,
-    style: 'tailwind',
+  addProjectConfiguration(tree, options.name, {
+    root: projectRoot,
+    projectType: 'library',
+    sourceRoot: `${projectRoot}/src`,
+    targets: {
+      'extract-tailwind': {
+        executor: '@spwntch/component-library-plugin:extract-tailwind',
+        options: {},
+      },
+      'add-docs': {
+        executor: '@spwntch/component-library-plugin:add-docs',
+        options: {},
+      },
+    },
   });
+
+  await addLinter(tree, projectRoot, options);
+  
+  // await libraryGenerator(tree, {
+  //   name: options.name,
+  //   directory: options.directory,
+  //   bundler: 'vite',
+  //   compiler: 'babel',
+  //   component: true,
+  //   linter: Linter.EsLint,
+  //   style: 'tailwind',
+  // });
   generateFiles(tree, join(__dirname, 'files'), projectRoot, options);
   await formatFiles(tree);
 }
