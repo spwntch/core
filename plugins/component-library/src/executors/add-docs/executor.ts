c20beeee3111cf6d8cf8821cf57ab6ab56eed6be
zpydee <benji@spawntech.io>
2024-07-07 13:05:19 +0200

    places generated library component in lib/components/...

--- plugins/component-library/src/executors/add-docs/executor.ts
+++ plugins/component-library/src/executors/add-docs/executor.ts
@@ -1,10 +1,15 @@
 import { PromiseExecutor } from '@nx/devkit';
 import { AddDocsExecutorSchema } from './schema';
+import { getExecutorCwd } from '../../lib/utils';
 
 const runExecutor: PromiseExecutor<AddDocsExecutorSchema> = async (options) => {
   const resolvedOptions = {
     ...options,
-    project: '${projectRoot}',
+    project: getExecutorCwd({
+      workspace: { projects: { ui: { root: 'libs/ui' } } },
+      projectName: 'ui',
+    }),
+    component: `src/lib/components/my-ui-component`,
   };
 
   console.log('Executor ran for AddDocs', resolvedOptions);
