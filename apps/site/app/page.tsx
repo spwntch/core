240aeb0a8874fab373c22ecce94b7a3f66a03a87
zpydee <benji@spawntech.io>
2024-07-10 11:12:18 +0200

    use @/utils cn

--- apps/site/app/page.tsx
+++ apps/site/app/page.tsx
@@ -1,7 +1,9 @@
+import { cn } from "@/utils";
+
 export default function Index() {
   return (
     <div className="container">
-      <h1 className="text-7xl text-primary">Hello World</h1>
+      <h1 className={cn("text-7xl text-primary")}>Hello World</h1>
     </div>
   );
 }
