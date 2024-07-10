48a0ea5d894c0a5740a1e1b67278bb5a3aed4f22
zpydee <benji@spawntech.io>
2024-07-10 11:17:30 +0200

    add extra arg to cn

--- apps/site/app/page.tsx
+++ apps/site/app/page.tsx
@@ -3,7 +3,7 @@ import { cn } from "@/utils";
 export default function Index() {
   return (
     <div className="container">
-      <h1 className={cn("text-7xl text-primary")}>Hello World</h1>
+      <h1 className={cn("text-7xl text-primary", "underline")}>Hello World</h1>
     </div>
   );
 }
